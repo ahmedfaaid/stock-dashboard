8369ac37008bf575b6ba46efcd06e6bbca5d4483
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-08-14 21:46:28 -0400

    set fixed height

--- src/components/CompInfo/CompInfo.styled.ts
+++ src/components/CompInfo/CompInfo.styled.ts
@@ -4,6 +4,8 @@ export const StyledCompanyContainer = styled.div`
   border-radius: 10px;
   background-color: ${({ theme }) => theme.colors.offWhite};
   width: 30%;
+  height: 320px;
+  max-height: 320px;
   padding: 15px;
   box-shadow: 3px 4px 3px ${({ theme }) => theme.colors.white};
 `;
