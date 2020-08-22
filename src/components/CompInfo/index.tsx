c90cc037e527415143ce2e8e797d3edd7cf12af1
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-08-21 22:26:46 -0400

    add tbody to tables

--- src/components/CompInfo/index.tsx
+++ src/components/CompInfo/index.tsx
@@ -17,30 +17,32 @@ export default function CompInfo({ currentStock }: Props) {
       </StyledImageContainer>
       <FullWidth>
         <table>
-          <tr>
-            <td>Symbol</td>
-            <StyledTableData>AAPL</StyledTableData>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <StyledTableData>Apple Inc.</StyledTableData>
-          </tr>
-          <tr>
-            <td>Stock Price</td>
-            <StyledTableData>$437.50</StyledTableData>
-          </tr>
-          <tr>
-            <td>Exchange</td>
-            <StyledTableData>NASDAQ/NMS (GLOBAL MARKET)</StyledTableData>
-          </tr>
-          <tr>
-            <td>IPO</td>
-            <StyledTableData>1980-12-12</StyledTableData>
-          </tr>
-          <tr>
-            <td>Industry</td>
-            <StyledTableData>Technology</StyledTableData>
-          </tr>
+          <tbody>
+            <tr>
+              <td>Symbol</td>
+              <StyledTableData>AAPL</StyledTableData>
+            </tr>
+            <tr>
+              <td>Name</td>
+              <StyledTableData>Apple Inc.</StyledTableData>
+            </tr>
+            <tr>
+              <td>Stock Price</td>
+              <StyledTableData>$437.50</StyledTableData>
+            </tr>
+            <tr>
+              <td>Exchange</td>
+              <StyledTableData>NASDAQ/NMS (GLOBAL MARKET)</StyledTableData>
+            </tr>
+            <tr>
+              <td>IPO</td>
+              <StyledTableData>1980-12-12</StyledTableData>
+            </tr>
+            <tr>
+              <td>Industry</td>
+              <StyledTableData>Technology</StyledTableData>
+            </tr>
+          </tbody>
         </table>
       </FullWidth>
     </StyledCompanyContainer>
--- src/components/CompStocks/index.tsx
+++ src/components/CompStocks/index.tsx
@@ -11,26 +11,28 @@ export default function CompStocks({ currentStock }: Props) {
     <StyledNewsContainer>
       <FullWidth style={{ textAlign: 'center' }}>
         <table style={{ width: 'max-content', margin: 'auto' }}>
-          <tr>
-            <td>Current Price</td>
-            <StyledTableData>$261.74</StyledTableData>
-          </tr>
-          <tr>
-            <td>Opening Price</td>
-            <StyledTableData>$261.07</StyledTableData>
-          </tr>
-          <tr>
-            <td>High Price</td>
-            <StyledTableData>$263.31</StyledTableData>
-          </tr>
-          <tr>
-            <td>Low Price</td>
-            <StyledTableData>$260.68</StyledTableData>
-          </tr>
-          <tr>
-            <td>Previous Close Price</td>
-            <StyledTableData>$259.45</StyledTableData>
-          </tr>
+          <tbody>
+            <tr>
+              <td>Current Price</td>
+              <StyledTableData>$261.74</StyledTableData>
+            </tr>
+            <tr>
+              <td>Opening Price</td>
+              <StyledTableData>$261.07</StyledTableData>
+            </tr>
+            <tr>
+              <td>High Price</td>
+              <StyledTableData>$263.31</StyledTableData>
+            </tr>
+            <tr>
+              <td>Low Price</td>
+              <StyledTableData>$260.68</StyledTableData>
+            </tr>
+            <tr>
+              <td>Previous Close Price</td>
+              <StyledTableData>$259.45</StyledTableData>
+            </tr>
+          </tbody>
         </table>
       </FullWidth>
     </StyledNewsContainer>
